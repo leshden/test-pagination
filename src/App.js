fa0cbfa243237de7468f64f40138a45eaea514e6
Viktor Denikin <leshden@yandex.ru>
2022-05-06 16:45:51 +0300

    posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Posts from './components/Posts';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
-    <div className="container">
-      <h1>App</h1>
+    <div className="container mt-5">
+      <h1 className='text-primary mb-3'>App</h1>
+      <Posts posts={currentPosts} loading={loading} />
     </div>
   );
 }
